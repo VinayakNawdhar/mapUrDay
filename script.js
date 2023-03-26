845bea90d0679c19ed8d4bffc27cd1f4a80e5cb0
Vinayak Nawdhar <vinayaknawdhar003@gmail.com>
2023-03-26 12:04:16 +0530

    approx finalised

--- script.js
+++ script.js
@@ -6,23 +6,16 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 class Workout {
   date = new Date();
   id = (Date.now() + ' ').slice(-10);
-  constructor(task, location, duration, timing, event) {
+  constructor(task, location, duration, timing, event,description) {
     this.task = task;
     this.location = location;
     this.duration = duration;
     this.timing = timing;
     this.event = event;
+    this.description = description;
   }
 }
 
-let events = [];
-
-if (localStorage.getItem('events')) {
-  events = console.parse(localStorage.getItem('events'));
-  events.forEach(el => {
-    markLocation(el.event);
-  });
-}
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -36,6 +29,7 @@ const workouts = document.querySelector('.workouts');
 class App {
   #map;
   #mapEvent;
+  #events = [];
 
   constructor() {
     this._getposition();
@@ -56,7 +50,7 @@ class App {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     const coords = [latitude, longitude];
-    this.#map = L.map('map').setView(coords, 12);
+    this.#map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
@@ -64,12 +58,15 @@ class App {
     }).addTo(this.#map);
 
     this.#map.on('click', this._showForm.bind(this));
+    this._getLocalStorage();
+    workouts.addEventListener('click', this._setViewFunction.bind(this));
   }
 
   _showForm(ev) {
     form.classList.toggle('hidden');
     inputTask.focus();
     this.#mapEvent = ev;
+    // console.log(ev);
   }
 
   _newWorkout(e) {
@@ -80,21 +77,23 @@ class App {
         inputLocation.value,
         +inputDuration.value,
         inputTiming.value,
-        this.#mapEvent
+        this.#mapEvent.latlng,
+        `${inputTask.value.toUpperCase()} at ${inputTiming.value}`
       );
+      // console.log(eventsObj);
       this._renderWorkoutMarker(eventsObj);
-      events.push(eventsObj);
+      this.#events.push(eventsObj);
       form.classList.toggle('hidden');
       clearFormField();
       this._addField(eventsObj);
-      workouts.addEventListener('click', this._setViewFunction.bind(this));
+      this._setLocalStorage();
     } else {
       alert('enter complete and correct details');
     }
   }
 
   _renderWorkoutMarker(eventsObj) {
-    L.marker([eventsObj.event.latlng.lat, eventsObj.event.latlng.lng])
+    L.marker([eventsObj.event.lat, eventsObj.event.lng])
       .addTo(this.#map)
       .bindPopup(
         L.popup({
@@ -105,7 +104,7 @@ class App {
           closeButton: false,
           className: 'running-popup',
         }).setContent(
-          `${inputTask.value.toUpperCase()} at ${inputTiming.value}`
+          eventsObj.description
         )
       )
       .openPopup();
@@ -149,10 +148,28 @@ class App {
   _setViewFunction(e) {
     const targetEl = e.target.closest('.workout');
     const id = targetEl.getAttribute('data-id');
-    const targetedWorkout = events.find(el => el.id === id);
-    const { lat, lng } = targetedWorkout.event.latlng;
-    this.#map.flyTo([lat, lng], 12);
+    const targetedWorkout = this.#events.find(el => el.id === id);
+    const { lat, lng } = targetedWorkout.event;
+    this.#map.flyTo([lat, lng], 14);
   }
+_setLocalStorage(){
+  localStorage.setItem('vinayak',JSON.stringify(this.#events));
+}
+_getLocalStorage(){
+  if (localStorage.getItem('vinayak')) {
+    this.#events = JSON.parse(localStorage.getItem('vinayak'));
+    console.log(this.#events);
+    for(let i=0;i<this.#events.length;i++){
+      this._addField(this.#events[i]);
+      this._renderWorkoutMarker(this.#events[i]);
+    }
+  }
+}
+reset(){
+  localStorage.clear();
+  location.reload();
+}
+
 }
 
 const app = new App();
@@ -162,5 +179,10 @@ function clearFormField() {
   inputLocation.value = '';
   inputDuration.value = '';
   inputTiming.value = '';
-  console.log(events);
+  // console.log(this.#events);
 }
+
+document.querySelector('.reset button').addEventListener('click',function(e){
+  e.preventDefault();
+  app.reset();
+})
